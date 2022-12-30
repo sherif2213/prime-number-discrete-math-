2bc78170b54b4cf5437503a4a0648707e78d0d25
sherif2213 <121685171+sherif2213@users.noreply.github.com>
2022-12-30 18:44:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("please enter first");
+
+			int first = int.Parse(Console.ReadLine());
+			Console.WriteLine("please enter second");
+
+			int second = int.Parse(Console.ReadLine());
+
+			bool prime = true;
+
+
+			for (int y = first; y < second; y++)
+			{
+				for (int o = 2; o < y; o++)
+				{
+					if (y % o == 0)
+					{
+
+						prime = false;
+						break;
+					}
+				}
+				if (prime == true)
+				{
+					Console.WriteLine(y);
+				}
+				else
+				{
+					prime = true;
+				}
+			}
+		}
+    }
+}
